8dfa888ca699636fb01886e36f45cb1703397634
Aleksandar Vulchanov <aleks.vulchanov@abv.bg>
2022-10-12 12:57:09 +0300

    Add jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,12 +16,25 @@ router.post('/register', async (req, res) => {
         return res.render('404');
     }
 
-    res.redirect('/auth/register')
+    res.redirect('/auth/login')
 });
 
 //Login
-router.get('/login', (req, res) => {
-    res.render('auth/login')
+router.get('/login', async (req, res) => {
+   await res.render('auth/login')
+});
+
+router.post('/login', async (req, res) => {
+
+   let token = await authService.login(req.body);
+   
+   if(!token){
+    //TODO: add notifications
+    return res.render('404');
+    }
+
+
+   res.redirect('/');
 });
 
 module.exports = router;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,12 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/User');
 
 const saltRounds = 10;
+const secret = 'eprogerihguowebrigweywuirguf';
 
+//REGISTER
 exports.register = async ({username, password, repeatPassword}) => {
     
     //TODO: form validation message
@@ -18,4 +22,31 @@ exports.register = async ({username, password, repeatPassword}) => {
     });
 
     return createdUser;  
+};
+
+//LOGIN
+exports.login = async ({username, password}) => {
+   let user = await User.findOne({username});
+
+   if(!user){
+        return;
+   }
+
+   const isValid = await bcrypt.compare(password, user.password);
+
+   if(!isValid){
+        return;
+   }
+
+   const result = new Promise((resolve, reject) => {
+       jwt.sign({_id: user._id, username: user.username}, secret, {expiresIn: '2d'}, (err, decodeToken) => {
+            if(err){
+                return reject(err);
+            }
+
+            resolve(decodeToken);
+       });
+   });
+
+   return result;
 };
